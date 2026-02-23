727041eb604b59f435ce79f043309b4d7fad5306
zYasuo <dansilvaagr@hotmail.com>
2026-02-23 10:22:18 -0300

    refactor: remove unused TypeScript build info, package lock, and various configuration files from the dl-speaks module to streamline the project structure

--- node_modules/endback/src/modules/dictionary/services/interfaces/dictionary-service.interface.ts
+++
@@ -1,5 +0,0 @@
-import type { TWordEntry } from "@shared/schemas/dictionary/words.schema";
-
-export interface IDictionaryService {
-    getWord(language: string, word: string): Promise<TWordEntry[]>;
-}
